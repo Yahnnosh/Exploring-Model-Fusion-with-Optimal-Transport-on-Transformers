43f43de4e7a387b750a9975f1549fff0a4d98e35
Janosch Jungo <jjungo@ethz.ch>
2022-12-22 11:32:59 +0100

    model_fusion2, utils now fully working

--- utils.py
+++ utils.py
@@ -12,6 +12,7 @@ from torchtext.vocab import build_vocab_from_iterator
 import re
 from transformers import BertTokenizer
 from torch.optim.lr_scheduler import ReduceLROnPlateau
+import ot
 
 from dataloader import *
 from transformer import *
@@ -47,6 +48,17 @@ def train_test_val_split(data, device, batch_size=512):
     return train_iter, valid_iter, test_iter, voc_size, pad_idx
 
 
+def check_imbalance(iterator, name=None):
+    all_y = np.array([])
+    for i, batch in enumerate(iterator):
+        _, trg = batch  # X, y
+        all_y = np.append(all_y, trg.cpu().numpy())
+    if name is not None:
+        print(f'Positive labels ratio ({name}):', np.mean(all_y))
+    else:
+        print('Positive labels ratio:', np.mean(all_y))
+
+
 # EXPORT DATA
 def save_model(model, name: str):
     torch.save(model, f'./Models/{name}')
@@ -58,6 +70,22 @@ def save_history(history, name: str):
 
 
 # MODEL FUNCTIONS
+def new_model(embedding, pad_idx, voc_size, device, max_len=256, d_model=16, ffn_hidden=32, n_head=1, n_layers=1,
+              drop_prob=0.5):
+    """Initializes a new model"""
+    model = TransformerClassifier(embedding=embedding,
+                                  src_pad_idx=pad_idx,
+                                  enc_voc_size=voc_size,
+                                  max_len=256,
+                                  d_model=16,
+                                  ffn_hidden=32,
+                                  n_head=1,
+                                  n_layers=1,
+                                  drop_prob=0.5,
+                                  device=device)
+    return model.to(device)  # put on CPU/GPU
+
+
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
@@ -141,7 +169,7 @@ def validation(model, iterator, optimizer, criterion, device):
     return val_epoch_loss / len(iterator), val_epoch_accuracy / len(iterator)
 
 
-def train(model, iterator, optimizer, criterion, epoch, clip, device):
+def train(model, iterator, valid_iter, optimizer, criterion, epoch, clip, device):
     # set model into training mode
     model.train()
 
@@ -277,6 +305,7 @@ def train_save_best(model, iterator, valid_iter, optimizer, criterion, epoch, cl
 
 # FUSION
 def weight_averaging(*weights):
+    device = weights[0].device
     with torch.no_grad():
         sum = torch.zeros(weights[0].shape, device=device)
         for weight in weights:
@@ -290,33 +319,27 @@ def linear_averaging(*linears):
         weights = [linear.weight for linear in linears]
         biases = [linear.bias for linear in linears]
 
+        device = weights[0].device
         linear_averaged = torch.nn.Linear(linears[0].in_features, linears[0].out_features, bias=True).to(device)
-        linear_averaged.weight, linear_averaged.bias = torch.nn.Parameter(weight_averaging(*weights)), torch.nn.Parameter(weight_averaging(*biases))
+        linear_averaged.weight, linear_averaged.bias = torch.nn.Parameter(weight_averaging(*weights)), \
+                                                       torch.nn.Parameter(weight_averaging(*biases))
 
     return linear_averaged
 
 
-def vanilla_fusion(modelA, modelB):
+def vanilla_fusion(modelA, modelB, pad_idx, voc_size, embedding, device):
     # init
-    model_fusion = TransformerClassifier(src_pad_idx = pad_idx,
-                                         enc_voc_size = voc_size,
-                                         max_len = 256,
-                                         d_model = 512,
-                                         ffn_hidden = 2048,
-                                         n_head = 1,
-                                         n_layers = 1,
-                                         drop_prob = 0.1,
-                                         device = device)
+    model_fusion = new_model(embedding, pad_idx, voc_size, device) # init model
 
     with torch.no_grad():
         # 1) encoder
         # TODO: smarter method for embedding
         # a) embedding
-        weights_A = modelA.encoder.emb.tok_emb.weight
+        '''weights_A = modelA.encoder.emb.tok_emb.weight
         weights_B = modelB.encoder.emb.tok_emb.weight
 
         weights_fusion = weight_averaging(weights_A, weights_B) # weights seem to be exactly the same?
-        model_fusion.encoder.emb.tok_emb.weight = torch.nn.Parameter(weights_fusion)
+        model_fusion.encoder.emb.tok_emb.weight = torch.nn.Parameter(weights_fusion)'''
 
         # b) encoder layers
         for i, _ in enumerate(modelA.encoder.layers):
@@ -350,7 +373,18 @@ def vanilla_fusion(modelA, modelB):
             model_fusion.encoder.layers[i].attention.w_concat = output_fusion
 
             # ii) layer norm 1
-            # TODO: LAYER NORM WEIGHTS ARE NOT CALLABLE???
+            # TODO: correct?
+            norm_gamma_A = modelA.encoder.layers[i].norm1.gamma
+            norm_gamma_B = modelB.encoder.layers[i].norm1.gamma
+
+            norm_gamma_fusion = weight_averaging(norm_gamma_A, norm_gamma_B)
+            model_fusion.encoder.layers[i].norm1.gamma = torch.nn.Parameter(norm_gamma_fusion)
+
+            norm_beta_A = modelA.encoder.layers[i].norm1.beta
+            norm_beta_B = modelB.encoder.layers[i].norm1.beta
+
+            norm_beta_fusion = weight_averaging(norm_beta_A, norm_beta_B)
+            model_fusion.encoder.layers[i].norm1.beta = torch.nn.Parameter(norm_beta_fusion)
 
             # iii) feed-forward network
             # layer 1
@@ -368,7 +402,18 @@ def vanilla_fusion(modelA, modelB):
             model_fusion.encoder.layers[i].ffn.linear2 = linear_fusion
 
             # iv) layer norm 2
-            # TODO: LAYER NORM WEIGHTS ARE NOT CALLABLE???
+            # TODO: correct?
+            norm_gamma_A = modelA.encoder.layers[i].norm2.gamma
+            norm_gamma_B = modelB.encoder.layers[i].norm2.gamma
+
+            norm_gamma_fusion = weight_averaging(norm_gamma_A, norm_gamma_B)
+            model_fusion.encoder.layers[i].norm2.gamma = torch.nn.Parameter(norm_gamma_fusion)
+
+            norm_beta_A = modelA.encoder.layers[i].norm2.beta
+            norm_beta_B = modelB.encoder.layers[i].norm2.beta
+
+            norm_beta_fusion = weight_averaging(norm_beta_A, norm_beta_B)
+            model_fusion.encoder.layers[i].norm2.beta = torch.nn.Parameter(norm_beta_fusion)
 
         # 2) MLP head
         linear_A = modelA.linear
@@ -377,14 +422,142 @@ def vanilla_fusion(modelA, modelB):
         linear_fusion = linear_averaging(linear_A, linear_B)
         model_fusion.linear = linear_fusion
 
+    print('fusion successful')
+    return model_fusion.to(device)
+
+
+def getSupport(model, trainloader, l, alignment="acts", numOfBatches=10):
+    '''
+    Get the support matrices using Activation-based ("acts") or Weight-based ("wts") alignment
+    '''
+    if alignment == "acts":
+        activation = None
+        for i, data in enumerate(trainloader, 0):
+            if i >= numOfBatches:
+                break
+
+            inputs, targets = data
+            outputs = model(inputs)
+
+            if activation is None:
+                activation = model.actMatrix[l]
+            else:
+                activation = torch.cat((activation, model.actMatrix[l]))
+
+        return activation
+
+    elif alignment == "wts":
+        return model.state_dict()[l]
+
+
+def fusion(modelA, modelB, weights_nameA, weights_nameB, weightsA, weightsB, transport_matrix, beta, train_iter):
+    """Aligns the parameters of model A and B and fuses them using OT"""
+    # Put parent models on cpu (if not already)
+    modelA = modelA.cpu()
+    modelB = modelB.cpu()
+
+    # Get Y-support (model B)
+    support_y = getSupport(modelB, train_iter, weights_nameB, alignment="wts")
+
+    # Align the weights from the first model
+    aligned_weights = torch.matmul(weightsA, torch.matmul(transport_matrix, torch.diag(1 / beta)))
+
+    # Get X-support (model A)
+    n = weightsA.shape[0]
+    alpha = torch.ones(n) * (1 / n)
+    support_x = getSupport(modelA, train_iter, weights_nameA, alignment="wts")
+
+    # Calculate the euclidean distance between the supports
+    distance = ot.dist(support_x, support_y)
+
+    # Calculate beta
+    m = weightsB.shape[0]
+    beta = torch.ones(m) * (1 / m)
+
+    # Calculate the transport matrix using optimal transport
+    transport_matrix = torch.from_numpy(ot.emd(alpha.numpy(), beta.numpy(), distance.detach().numpy())).float().reshape(
+        (n, m))
+
+    # Align model neurons
+    aligned_model = torch.matmul(torch.diag(1 / beta), torch.matmul(transport_matrix.T, aligned_weights))
+
+    # Get the weights at layer "idx" from the second model
+    fused = (aligned_model + weightsB) / 2
+
+    return fused, transport_matrix, beta
+
+
+def ot_fusion(modelA, modelB, train_iter, embedding, pad_idx, voc_size, device, fusion_ratio=0.5):
+    """Fuses models A, B together using optimal transport"""
+    # Initialize new model
+    model_fusion = new_model(embedding, pad_idx, voc_size, device) # init model
+    a = fusion_ratio
+
+    # Initialize fused weights dictionary
+    W_fusion = dict.fromkeys(list(modelA.state_dict().keys()))
+
+    # Initialize the algorithm
+    m = list(modelB.state_dict().items())[1][1].shape[1]
+    beta = torch.ones(m) * (1 / m)
+    transport_matrix = torch.matmul(torch.diag(beta), torch.eye(m))
+
+    # Fusion via Optimal Transport
+    for (nameA, weightA), (nameB, weightB) in zip(modelA.named_parameters(), modelB.named_parameters()):
+        if nameA == "encoder.emb.tok_emb.embedding.weight":
+            W_fusion[nameA] = weightA  # all models use same embedding
+        else:
+            if "weight" in nameA:
+                if "encoder" in nameA:
+                    W_fusion[nameA], transport_matrix, beta = fusion(modelA, modelB, nameA, nameB, weightA, weightB,
+                                                                     transport_matrix, beta, train_iter)
+                else:
+                    W_fusion[nameA] = a * weightA + (1 - a) * weightB
+            elif "bias" in nameA:
+                if "encoder" in nameA:
+                    # we transport the biases the same way we transported the neurons (weight matrices)
+                    m = weightB.shape[0]
+                    beta_bias = torch.ones(m) * (1 / m)
+
+                    W_A_bias = weightA.reshape(m, 1)
+
+                    aligned_bias = torch.matmul(torch.diag(1 / beta_bias), torch.matmul(transport_matrix.T, W_A_bias))
+                    aligned_bias = aligned_bias.reshape(m)
+
+                    W_fusion[nameA] = (aligned_bias + weightB) / 2
+                else:
+                    W_fusion[nameA] = a * weightA + (1 - a) * weightB
+            else:
+                W_fusion[nameA] = a * weightA + (1 - a) * weightB
+
+    # Assign the weights
+    with torch.no_grad():
+        for name, param in model_fusion.named_parameters():
+            param.data = torch.nn.Parameter(W_fusion[name])
+
+    print('fusion successful')
     return model_fusion
 
 
-def test_fusion(modelA, modelB, model_fusion):
+def test_fusion(modelA, modelB, model_fusion, test_iter, device):
+    # ensure all models on same device
+    modelA = modelA.to(device)
+    modelB = modelB.to(device)
+    model_fusion = model_fusion.to(device)
+
     # test fusion
     test_loss_A, test_acc_A = validation(modelA, test_iter, None, nn.CrossEntropyLoss(), device)
     test_loss_B, test_acc_B = validation(modelB, test_iter, None, nn.CrossEntropyLoss(), device)
     test_loss_fusion, test_acc_fusion = validation(model_fusion, test_iter, None, nn.CrossEntropyLoss(), device)
+
+    # put into cpu
+    test_loss_A = test_loss_A.to('cpu') if isinstance(test_loss_A, torch.Tensor) else test_loss_A
+    test_acc_A = test_acc_A.to('cpu') if isinstance(test_acc_A, torch.Tensor) else test_acc_A
+
+    test_loss_B = test_loss_B.to('cpu') if isinstance(test_loss_B, torch.Tensor) else test_loss_B
+    test_acc_B = test_acc_B.to('cpu') if isinstance(test_acc_B, torch.Tensor) else test_acc_B
+
+    test_loss_fusion = test_loss_fusion.to('cpu') if isinstance(test_loss_fusion, torch.Tensor) else test_loss_fusion
+    test_acc_fusion = test_acc_fusion.to('cpu') if isinstance(test_acc_fusion, torch.Tensor) else test_acc_fusion
 
     # visualize
     fig, ax = plt.subplots()
@@ -393,18 +566,27 @@ def test_fusion(modelA, modelB, model_fusion):
     metrics_B = [test_loss_B, test_acc_B]
     metrics_fusion = [test_loss_fusion, test_acc_fusion]
     metrics = ['loss', 'accuracy']
-    x = np.arange(len(metrics)) # positions of bars (1 per metric)
+    x = np.arange(len(metrics))  # positions of bars (1 per metric)
     width = 0.25  # the width of the bars
 
+    # bars
     rects1 = ax.bar(x - width, metrics_A, width, label='model A')
     rects2 = ax.bar(x, metrics_B, width, label='model B')
     rects3 = ax.bar(x + width, metrics_fusion, width, label='model fusion')
+
+    # number on top of bars
+    for rect in (-1, 0, 1): # for each bar
+        metric = [metrics_A, metrics_B, metrics_fusion][rect + 1] # choose values for model A, B or fusion
+        for i, x_ in enumerate(x + rect * width): # x_ is position of bar
+            y_ = metric[i] if not isinstance(metric[i], torch.Tensor) else metric[i].numpy()
+            plt.text(x=x_ - 0.3 * width, y=y_ + 0.03, s=np.round(y_, 3))
 
     ax.set_ylabel('Score')
     ax.set_title('Test metrics by models')
     ax.set_xticks(x)
     ax.set_xticklabels(metrics)
     ax.legend()
+    plt.ylim([0, max([max(element) for element in [metrics_A, metrics_B, metrics_fusion]]) + 0.2])
 
     plt.show()
 
