dac4433eb0b53632053326ea255b70ebd2847c10
Janosch Jungo <jjungo@ethz.ch>
2023-01-04 21:20:45 +0100

    prettier CV (normal + weighted) for different_seeds

--- utils.py
+++ utils.py
@@ -29,6 +29,96 @@ def import_data():
     return data
 
 
+def preproc(dat):
+    data = dat.copy()
+
+    # proc
+    data.iloc[:, 0] = data.iloc[:, 0].apply(lambda x: x.replace("<br />", " "))  # remove break symbols
+    data.iloc[:, 0] = data.iloc[:, 0].apply(lambda x: x.lower())  # all lower case
+
+    # only use short sentences
+    data["len"] = data.iloc[:, 0].apply(lambda x: len(x.split()))
+    data = data[data["len"] < 256]
+
+    # tokenize data (split sentence into tokens)
+    tokenizer = Tokenizer()
+    print("Tokenizing the data...")
+    data["len"] = data.iloc[:, 0].apply(lambda x: len(tokenizer.tokenize(x)))
+
+    # only use short sentences
+    data = data[data["len"] < 256]
+
+    # dataset size
+    print("Length of the data : ", len(data))
+
+    # further proc
+    data.iloc[:, 0] = data.iloc[:, 0].apply(
+        lambda row: re.sub("[^A-Za-z]+", " ", row)).apply(tokenizer.tokenize)
+
+    # reset index (we deleted some rows)
+    data = data.reset_index(drop=True)
+
+    return data
+
+
+def build_generators(train, test, device, batch_size=512):
+    """Builds training and test generators from specific datasets"""
+    # build vocab
+    vocab = {'__PAD__': 0, '__</e>__': 1, '__UNK__': 2}
+    for item in train.iloc[:, 0]:
+        for word in item:
+            if word not in vocab:
+                vocab[word] = len(vocab)
+    pad_idx = vocab['__PAD__']
+    voc_size = len(vocab)
+    print("Vocabulary Size : ", voc_size)
+
+    # create embedding
+    embedding = torch.nn.Embedding(voc_size, 16)
+
+    # dataframe to tensor
+    train_y = torch.tensor(train.iloc[:, 1].values.astype(np.float32), device=device)
+    test_y = torch.tensor(test.iloc[:, 1].values.astype(np.float32), device=device)
+
+    unk_ID = vocab["__UNK__"]
+
+    train_x_tensor = []
+    for idx, text_corpus in enumerate(tqdm(train.iloc[:, 0])):
+        foo = []
+        for token in text_corpus:
+            word_ID = vocab.get(token, unk_ID)
+            foo.append(word_ID)
+        while len(foo) < 256:
+            foo.append(vocab["__PAD__"])
+        train_x_tensor.append(foo)
+
+    test_x_tensor = []
+    for idx, text_corpus in enumerate(tqdm(test.iloc[:, 0])):
+        foo = []
+        for token in text_corpus:
+            word_ID = vocab.get(token, unk_ID)
+            foo.append(word_ID)
+        while len(foo) < 256:
+            foo.append(vocab["__PAD__"])
+        test_x_tensor.append(foo)
+
+    train_x = torch.tensor(train_x_tensor, device=device)
+    test_x = torch.tensor(test_x_tensor, device=device)
+
+    train = torch.utils.data.TensorDataset(train_x, train_y)
+    test = torch.utils.data.TensorDataset(test_x, test_y)
+
+    train_iterator = torch.utils.data.DataLoader(dataset=train, batch_size=batch_size, shuffle=True)
+    test_iterator = torch.utils.data.DataLoader(dataset=test, batch_size=batch_size, shuffle=True)
+
+    # check imbalance
+    check_imbalance(train_iterator, name='train set')
+    check_imbalance(test_iterator, name='test set')
+
+    print('Dataset initializing done')
+    return train_iterator, test_iterator, voc_size, pad_idx, embedding
+
+
 def train_test_val_split(data, device, batch_size=512):
     # tokenizer (split sentence into tokens)
     tokenizer = Tokenizer()
@@ -138,7 +228,7 @@ def plot_training(history, marker=None):
     plt.show()
 
 
-def validation(model, iterator, optimizer, criterion, device):
+def validation(model, iterator, criterion, device):
     # set model into evaluation mode
     model.eval()
 
@@ -184,7 +274,7 @@ def train(model, iterator, valid_iter, optimizer, criterion, epoch, clip, device
     assert isinstance(epoch, int) or epoch == 'unrestricted', f'Invalid epoch: {epoch}'
     unrestricted = False
     if epoch == 'unrestricted':
-        epoch = int(1e6) # large enough
+        epoch = int(1e6)  # large enough
         unrestricted = True
 
     # set model into training mode
@@ -524,10 +614,10 @@ def fusion(modelA, modelB, weights_nameA, weights_nameB, weightsA, weightsB, tra
     return fused, transport_matrix, beta
 
 
-def ot_fusion(modelA, modelB, train_iter, embedding, pad_idx, voc_size, device, fusion_ratio=0.5):
+def ot_fusion(modelA, modelB, train_iter, embedding, pad_idx, voc_size, device, fusion_ratio=0.5, drop_prob=0.5):
     """Fuses models A, B together using optimal transport"""
     # Initialize new model
-    model_fusion = new_model(embedding, pad_idx, voc_size, device)  # init model
+    model_fusion = new_model(embedding, pad_idx, voc_size, device, drop_prob=0.5)  # init model
     a = fusion_ratio
 
     # Initialize fused weights dictionary
@@ -547,7 +637,8 @@ def ot_fusion(modelA, modelB, train_iter, embedding, pad_idx, voc_size, device, 
                 if "encoder" in nameA:
                     if "concat" not in nameA and "linear" not in nameA:
                         # query, key and value matrices get the same transport matrix
-                        W_fusion[nameA], transport_matrix_triplet, _ = fusion(modelA, modelB, nameA, nameB, weightA, weightB,
+                        W_fusion[nameA], transport_matrix_triplet, _ = fusion(modelA, modelB, nameA, nameB, weightA,
+                                                                              weightB,
                                                                               transport_matrix, beta, train_iter)
                     else:
                         W_fusion[nameA], transport_matrix, beta = fusion(modelA, modelB, nameA, nameB, weightA, weightB,
@@ -562,7 +653,8 @@ def ot_fusion(modelA, modelB, train_iter, embedding, pad_idx, voc_size, device, 
 
                         W_A_bias = weightA.reshape(m, 1)
 
-                        aligned_bias = torch.matmul(torch.diag(1 / beta_bias), torch.matmul(transport_matrix_triplet.T, W_A_bias))
+                        aligned_bias = torch.matmul(torch.diag(1 / beta_bias),
+                                                    torch.matmul(transport_matrix_triplet.T, W_A_bias))
                         aligned_bias = aligned_bias.reshape(m)
 
                         W_fusion[nameA] = (aligned_bias + weightB) / 2
@@ -572,7 +664,8 @@ def ot_fusion(modelA, modelB, train_iter, embedding, pad_idx, voc_size, device, 
 
                         W_A_bias = weightA.reshape(m, 1)
 
-                        aligned_bias = torch.matmul(torch.diag(1 / beta_bias), torch.matmul(transport_matrix.T, W_A_bias))
+                        aligned_bias = torch.matmul(torch.diag(1 / beta_bias),
+                                                    torch.matmul(transport_matrix.T, W_A_bias))
                         aligned_bias = aligned_bias.reshape(m)
 
                         W_fusion[nameA] = (aligned_bias + weightB) / 2
@@ -641,3 +734,128 @@ def test_fusion(modelA, modelB, model_fusion, test_iter, device):
     plt.ylim([0, max([max(element) for element in [metrics_A, metrics_B, metrics_fusion]]) + 0.2])
 
     plt.show()
+
+
+# wrapper function to optimize weighting factor
+def weighted_fusion(modelA, modelB, train_iter, valid_iter, embedding, pad_idx, voc_size, device):
+    def objective(trial):
+        weighting_factor = trial.suggest_float('weighting_factor', 0, 1)
+
+        # weighted fusion
+        model_fusion = ot_fusion(modelA, modelB, train_iter, embedding, pad_idx, voc_size, device, fusion_ratio=weighting_factor)
+        model_fusion.to(device)
+
+        # validate fusion model
+        criterion = torch.nn.CrossEntropyLoss()
+        val_loss, val_acc, f1 = validation(model_fusion, valid_iter, None, criterion, device)
+
+        return val_loss
+    return objective
+
+
+def calibrate_norm_layer(fusedModel, train_iter, device, learning_rate=0.01):
+    optimizer = torch.optim.Adam(fusedModel.parameters(), lr=learning_rate)
+    criterion = torch.nn.CrossEntropyLoss()
+
+    clip = 1
+
+    fusedModel.train()
+    for name, param in fusedModel.named_parameters():
+        if "weight" in name or "bias" in name:
+            param.requires_grad = False
+
+    for i, batch in enumerate(tqdm(train_iter)):
+        src = batch[0] # X
+        trg = batch[1] # y
+        src, trg = torch.tensor(src).to(device), torch.tensor(trg).to(device) # put to cpu/gpu
+        optimizer.zero_grad() # reset optimizer
+        output = fusedModel(src) # predict
+        y_pred = torch.argmax(output, dim=-1) # logits -> labels
+        output_reshape = output.contiguous().view(-1, output.shape[-1])
+        trg = trg.to(torch.int64)
+        loss = criterion(output_reshape, trg) # calculate loss
+        agreements = torch.eq(y_pred, trg)
+        accuracy = torch.mean(agreements.double()) # calculate accuracy
+        loss.backward() # backward pass
+
+        torch.nn.utils.clip_grad_norm_(fusedModel.parameters(), clip)
+        optimizer.step() # optimize model
+
+    for name, param in fusedModel.named_parameters():
+        param.requires_grad = True
+
+
+def train_last_layer(fusedModel, train_iter, device, learning_rate=1e-3):
+    optimizer = torch.optim.Adam(fusedModel.parameters(), lr=learning_rate)
+    criterion = torch.nn.CrossEntropyLoss()
+
+    clip = 1
+
+    fusedModel.train()
+    for name, param in fusedModel.named_parameters():
+        if "encoder" in name:
+            param.requires_grad = False
+
+    for i, batch in enumerate(tqdm(train_iter)):
+        src = batch[0] # X
+        trg = batch[1] # y
+        src, trg = torch.tensor(src).to(device), torch.tensor(trg).to(device) # put to cpu/gpu
+        optimizer.zero_grad() # reset optimizer
+        output = fusedModel(src) # predict
+        y_pred = torch.argmax(output, dim=-1) # logits -> labels
+        output_reshape = output.contiguous().view(-1, output.shape[-1])
+        trg = trg.to(torch.int64)
+        loss = criterion(output_reshape, trg) # calculate loss
+        agreements = torch.eq(y_pred, trg)
+        accuracy = torch.mean(agreements.double()) # calculate accuracy
+        loss.backward() # backward pass
+
+        torch.nn.utils.clip_grad_norm_(fusedModel.parameters(), clip)
+        optimizer.step() # optimize model
+
+
+# TEST FUNCTIONS
+def scores_to_latex(scores, model_names):
+    df = pd.DataFrame.from_dict(scores)
+
+    # for visualization
+    temp = pd.DataFrame.from_dict(scores)
+    n_rows, n_cols = temp.shape
+    for row in range(n_rows):
+        for col in range(n_cols):
+            entry = temp.iloc[row, col]
+            temp.iloc[row, col] = [np.round(element, 3) if not isinstance(element, torch.Tensor) else np.round(element.numpy(), 3) for element in entry]
+    print(temp)
+
+    # entries like: mean +- std
+    n_rows, n_cols = df.shape
+    for row in range(n_rows):
+        df.iloc[row, 0] = str(np.round(np.mean(df.iloc[row, 0]), 3)) + ' ± ' + str(np.round(np.std(df.iloc[row, 0]), 3)) # loss
+        df.iloc[row, 1] = str(np.round(np.mean(df.iloc[row, 1]), 3)) + ' ± ' + str(np.round(np.std(df.iloc[row, 1]), 3)) # accuracy
+        df.iloc[row, 2] = str(np.round(np.mean(df.iloc[row, 2]), 3)) + ' ± ' + str(np.round(np.std(df.iloc[row, 2]), 3)) # f1
+
+    # rename rows, cols
+    df.columns = ['Loss', 'Accuracy', 'F1 score']
+    df.index = model_names
+
+    # boldify highest score
+    for col in (0, 1, 2):
+        if col == 0:
+            index_max = np.argmin([float(entry.split('±')[0]) for entry in df.iloc[:, col]])
+        else:
+            index_max = np.argmax([float(entry.split('±')[0]) for entry in df.iloc[:, col]])
+        entry = df.iloc[index_max, col]
+        entry = 'BOLD{' + entry + '}'
+        df.iloc[index_max, col] = entry
+
+
+    # convert to latex
+    latex = df.to_latex(index=True,
+                        bold_rows=True,
+                        caption='Model performance (5-fold CV)',
+                        position='H').replace('BOLD\\', r'\textbf').replace('\}', '}')
+
+    # print as latex
+    print(latex)
+
+    return latex
